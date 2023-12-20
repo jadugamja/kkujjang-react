22ead3b532d0521d66f430b46312cdff4d929201
jasook <gheddong2@naver.com>
2023-12-20 23:07:10 +0900

    Add: 기본 폰트 Noto Sans KR 적용 - CommonStyle.jsx: FlexBox, SubmitButton 추가

--- src/styles/CommonStyle.jsx
+++ src/styles/CommonStyle.jsx
@@ -1,5 +1,12 @@
 import styled from "styled-components";
 
+export const FlexBox = styled.div`
+  display: flex;
+  ${(props) => setDirection(props.dir)}
+  ${(props) => setHorizontalAlign(props.row)}
+  ${(props) => setVerticalAlign(props.col)}
+`;
+
 export const Gradation = styled.div`
   position: absolute;
   top: 0;
@@ -8,3 +15,74 @@ export const Gradation = styled.div`
   background-image: linear-gradient(#dbe1ed, #00000000);
   z-index: -1;
 `;
+
+export const SubmitButtonWrapper = styled(FlexBox)`
+  width: 100%;
+  height: 75px;
+`;
+
+export const SubmitButton = styled.button`
+  width: 100%;
+  height: inherit;
+  background-color: ${(props) => props.theme.color.button.main.background};
+  color: ${(props) => props.theme.color.button.main.text};
+  font-size: ${(props) => props.theme.fontSize.xl};
+`;
+
+// flex-box의 flex-direction 설정
+const setDirection = (dir) => {
+  switch (dir) {
+    case "col":
+      return `
+              flex-direction: column;
+          `;
+  }
+};
+
+// flex-box 안의 justify-content 값 설정
+const setHorizontalAlign = (row) => {
+  switch (row) {
+    case "center":
+      return `
+              justify-content: center;
+          `;
+    case "start":
+      return `
+              justify-content: flex-start;
+          `;
+    case "end":
+      return `
+              justify-content: flex-end;
+          `;
+    case "between":
+      return `
+              justify-content: space-between;
+          `;
+  }
+};
+
+// flex-box 안의 align-items 값 설정
+const setVerticalAlign = (col) => {
+  switch (col) {
+    case "center":
+      return `
+              align-items: center;
+          `;
+    case "start":
+      return `
+              align-items: flex-start;
+          `;
+    case "end":
+      return `
+              align-items: flex-end;
+          `;
+    case "baseline":
+      return `
+              align-items: baseline;
+          `;
+    case "stretch":
+      return `
+              align-items: stretch;
+          `;
+  }
+};
--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,15 +1,36 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'Noto Sans KR';
+    src: url('/fonts/NotoSansKR-VariableFont_wght.ttf') format('truetype');
+    font-weight: 100 900;
+  }
+
+  @font-face {
+    font-family: 'Pretendard Variable';
+    src: url('/fonts/PretendardVariable.woff2') format('woff2-variations');
+    font-weight: 100 900;
+  }
+
   * {
     -webkit-box-sizing: border-box;
     -moz-box-sizing: border-box;
     box-sizing: border-box;
+    font-family: 'Noto Sans KR', sans-serif, 'FontAwesome';
   }
 
   body {
     margin: 0;
     background: ${(props) => props.theme.color.background}
+  }
+
+  form {
+    margin: 0;
+  }
+
+  a:hover, button:hover, input[type="submit"] {
+    cursor: pointer;
   }
 `;
 

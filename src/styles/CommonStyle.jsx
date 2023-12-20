e0631ce7de37db1db56209c7b899e625877f251d
jasook <gheddong2@naver.com>
2023-12-20 21:25:54 +0900

    Style: 공통 스타일 설정 theme.js: 반복절으로 쓰이는 color, font-size에 대한 값 적용 GlobalStyle.js: 공통 css 스타일 적용, Web과 Game 각각의 배경색 적용 CommonStyle.jsx: Web 내에서 공통적으로 쓰이는 Gradation 스타일 태그 추가

---
+++ src/styles/CommonStyle.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Gradation = styled.div`
+  position: absolute;
+  top: 0;
+  width: 100%;
+  height: 42.25rem;
+  background-image: linear-gradient(#dbe1ed, #00000000);
+  z-index: -1;
+`;

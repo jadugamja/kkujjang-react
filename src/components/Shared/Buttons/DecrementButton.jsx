108ad5ac2357f8d3336d07904acc84da892cc860
김해원 <haewon@gimhaewon-ui-MacBookPro.local>
2024-01-01 21:39:13 +0900

    Feat & Modify: Button 컴포넌트 생성 및 IncrementButton, DecrementButton Style 추가 1. CommonStyle에서 Button 관련된 내용 분리하여 ButtonStyle에 추가함 2. Button 컴포넌트 생성 : type, message를 props로 받음 3. IncrementButton, DecrementButton Style은 별도의 파일로 분리하지 않고, ButtonStyle에서 처리함

--- src/components/Shared/Buttons/DecrementButton.jsx
+++ src/components/Shared/Buttons/DecrementButton.jsx
@@ -3,18 +3,19 @@ import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
 
+import { ButtonWrapper } from "./ButtonStyle";
+
 const DecrementButton = () => {
-  const [count, setCount] = useState(0);
+  const [number, setNumber] = useState(0);
 
   const handleDecrease = () => {
-    setCount(count - 1);
+    setNumber(number - 1);
   };
 
-  // 증감소 버튼 style CommonStyle에 있는지
   return (
-    <button onClick={handleDecrease}>
-      <FontAwesomeIcon icon={faCaretDown} />
-    </button>
+    <ButtonWrapper onClick={handleDecrease}>
+      <FontAwesomeIcon icon={faCaretDown} size="2x" />
+    </ButtonWrapper>
   );
 };
 

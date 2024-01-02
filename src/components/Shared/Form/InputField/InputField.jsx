28d7933f723bd450a415d9439e4eca0e7e56ab59
김해원 <haewon@gimhaewon-ui-MacBookPro.local>
2024-01-02 17:54:52 +0900

    Feat & Modify: PasswordRequirement 컴포넌트 내부 로직 수정 및 InputField 컴포넌트에 onChange 이벤트 추가함

--- src/components/Shared/Form/InputField/InputField.jsx
+++ src/components/Shared/Form/InputField/InputField.jsx
@@ -4,30 +4,83 @@ import PropTypes from "prop-types";
 import { Label, Input } from "./InputFieldStyle";
 
 const InputField = ({ type, message }) => {
+  const [idValue, setIdValue] = useState("");
+  const [pwValue, setPwValue] = useState("");
+  const [pwCheckValue, setpwCheckValue] = useState("");
+
+  const handleInputAboutIdChange = (e) => {
+    setIdValue(e.target.value);
+  };
+
+  const handleInputAboutPwChange = (e) => {
+    setPwValue(e.target.value);
+  };
+
+  const handleInputAboutPwCheckChange = (e) => {
+    setpwCheckValue(e.target.value);
+  };
+
   return (
     <>
       {/* {Label 포함한 Input (회원가입 등)} */}
       {type === "label_id" && (
         <Label>
           아이디
-          <Input type="text" placeholder="아이디 입력" />
+          <Input
+            type="text"
+            value={idValue}
+            onChange={handleInputAboutIdChange}
+            placeholder="아이디 입력"
+          />
         </Label>
       )}
       {type === "label_pw" && (
         <Label>
           비밀번호
-          <Input type="password" placeholder="비밀번호 입력" />
+          <Input
+            type="password"
+            value={pwValue}
+            onChange={handleInputAboutPwChange}
+            placeholder="비밀번호 입력"
+          />
         </Label>
       )}
       {type === "label_pwCheck" && (
         <Label>
           비밀번호 확인
-          <Input type="password" placeholder="비밀번호 확인" />
+          <Input
+            type="password"
+            value={pwCheckValue}
+            onChange={handleInputAboutPwCheckChange}
+            placeholder="비밀번호 확인"
+          />
         </Label>
       )}
       {/* {Label 없는 Input} */}
-      {type === "id" && <Input type="text" placeholder="아이디를 입력하세요" />}
-      {type === "pw" && <Input type="password" placeholder={message} />}
+      {type === "id" && (
+        <Input
+          type="text"
+          value={idValue}
+          onChange={handleInputAboutIdChange}
+          placeholder="아이디를 입력하세요"
+        />
+      )}
+      {type === "pw" && (
+        <Input
+          type="password"
+          value={pwValue}
+          onChange={handleInputAboutPwChange}
+          placeholder={message}
+        />
+      )}
+      {type === "pwChaeck" && (
+        <Input
+          type="password"
+          value={pwCheckValue}
+          onChange={handleInputAboutPwCheckChange}
+          placeholder={message}
+        />
+      )}
     </>
   );
 };
--- src/components/Shared/Form/Validation/PasswordRequirement/PasswordRequirement.jsx
+++ src/components/Shared/Form/Validation/PasswordRequirement/PasswordRequirement.jsx
@@ -1,54 +1,48 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-import PasswordRequirementWrapper from "./PasswordRequirementStyle.jsx";
-import PasswordRequirementMeassage from "./PasswordRequirementStyle.jsx";
+import {
+  PasswordRequirementWrapper,
+  PasswordRequirementMeassage
+} from "./PasswordRequirementStyle";
 
 const PasswordRequirement = (props) => {
-  let message = "";
+  const { type } = props;
+  const password = props.pwValue;
+  const passwordCheck = props.pwCheckValue;
 
-  const [isValid, setIsValid] = useState(false);
-
-  const { type, inputValue, isMatch } = props;
-
-  if (type === "uppercaseAndLowercaseLetter") {
-    message = "영어 대소문자";
-  } else if (type === "number") {
-    message = "숫자";
-  } else if (type === "specialCharacter") {
-    message = "특수문자";
-  } else if (type === "length") {
-    message = "7-30자 이내";
-  } else if (type === "confirmPassword") {
-    message = "비밀번호 일치";
-  }
-
-  useEffect(() => {
-    const validate = () => {
-      if (type === "uppercaseAndLowercaseLetter") {
-        return /[A-Za-z]/.test(inputValue);
-      } else if (type === "number") {
-        return /\d/.test(inputValue);
-      } else if (type === "specialCharacter") {
-        return /[!@#$%^&*(),.?":{}|<>]/.test(inputValue);
-      } else if (type === "length") {
-        return inputValue.length >= 7 && inputValue.length <= 30;
-      } else if (type === "confirmPassword") {
-        return isMatch;
-      }
-
-      return false;
-    };
-
-    setIsValid(validate());
-  }, [type, inputValue, isMatch]);
+  // 비밀번호 조건 정규표현식
+  // const isLengthValid = password.length >= 7 && password.length <= 30;
+  // const hasNumber = /\d/.test(password);
+  // const hasUppercaseAndLowercase = /[A-Za-z]/.test(password);
+  // const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+  // const isMatch = password === passwordCheck;
 
   return (
     <PasswordRequirementWrapper>
-      <PasswordRequirementMeassage color={isValid ? true : false}>
-        {message}
-      </PasswordRequirementMeassage>
+      {type === "english" && (
+        <PasswordRequirementMeassage>영어 대소문자</PasswordRequirementMeassage>
+      )}
+      {type === "number" && (
+        <PasswordRequirementMeassage>숫자</PasswordRequirementMeassage>
+      )}
+      {type === "specialCharacter" && (
+        <PasswordRequirementMeassage>특수문자</PasswordRequirementMeassage>
+      )}
+      {type === "lenght" && (
+        <PasswordRequirementMeassage>7-30자 이내</PasswordRequirementMeassage>
+      )}
+      {type === "match" && (
+        <PasswordRequirementMeassage>비밀번호 일치</PasswordRequirementMeassage>
+      )}
     </PasswordRequirementWrapper>
   );
+};
+
+PasswordRequirement.propTypes = {
+  type: PropTypes.string,
+  pwValue: PropTypes.string,
+  pwCheckValue: PropTypes.string
 };
 
 export default PasswordRequirement;

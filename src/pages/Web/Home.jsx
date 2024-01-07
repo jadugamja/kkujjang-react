ea2514472d1ba488c8fcbf25eaf018c05b92d2df
김해원 <haewon@gimhaewon-ui-MacBookPro.local>
2024-01-07 15:54:33 +0900

    Modify: Home.jsx에서 test 코드 삭제함

--- src/pages/Web/Home.jsx
+++ src/pages/Web/Home.jsx
@@ -4,7 +4,6 @@ import { Outlet, useLocation } from "react-router-dom";
 import { Gradation, ContentWrapper, WideContent, Main } from "@/styles/CommonStyle";
 import Header from "@/components/Web/Shared/Layout/Header";
 import Footer from "@/components/Web/Shared/Layout/Footer";
-import InputField from "@/components/Web/Shared/Form/InputField";
 
 const Home = () => {
   const location = useLocation();
@@ -26,8 +25,6 @@ const Home = () => {
           <WideContent dir="col">
             <Header />
             <Main></Main>
-            <InputField name="password" hasLabel={true} logInForm={true}></InputField>
-            <InputField name="password_check" hasLabel={true}></InputField>
           </WideContent>
         </ContentWrapper>
       )}

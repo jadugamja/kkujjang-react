6fc79540774c0cfcb3d9341cb5c5d2c024e964ed
김해원 <haewon@gimhaewon-ui-MacBookPro.local>
2024-02-24 17:46:00 +0900

    Feat: InquiryCreate, InquiryList 페이지 구현 & Fix: BoardItem, BoardTitle 수정 1. InquiryList를 구현하기 위해 ThreadItem, Thread 컴포넌트 생성 2. 스레드에서 입력 시에 새로 백엔드 통신하는 것으로 결정

--- src/pages/Web/Inquiry/InquiryList.jsx
+++ src/pages/Web/Inquiry/InquiryList.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import styled from "styled-components";
+
+import { ContentWrapper, WideContent, Main } from "@/styles/CommonStyle";
+import Header from "@/components/Web/Shared/Layout/Header";
+import InquiryListContainer from "@/components/Web/Support/InquiryListContainer";
+
+const InquiryListMain = styled(Main)`
+  background-color: ${({ theme }) => theme.colors.content};
+  height: 100vh;
+`;
 
 const InquiryList = () => {
-  return <h1>IInquiryList Page</h1>;
+  return (
+    <ContentWrapper row="center" col="center">
+      <Header type="detail" />
+      <WideContent dir="col" width="100%">
+        <InquiryListMain>
+          <InquiryListContainer />
+        </InquiryListMain>
+      </WideContent>
+    </ContentWrapper>
+  );
 };
 
 export default InquiryList;

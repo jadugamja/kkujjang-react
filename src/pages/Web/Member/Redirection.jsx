f33ad9654f2b3fa2093f7cac0864c53d40e81c76
김해원 <haewon@gimhaewon-ui-MacBookPro.local>
2024-04-02 15:14:46 +0900

    Conflict: merge 후 conflict 해결

--- src/pages/Web/Member/Redirection.jsx
+++ src/pages/Web/Member/Redirection.jsx
@@ -8,13 +8,18 @@ import useAxios from "@/hooks/useAxios";
 // ===== component =====
 const Redirection = () => {
   const code = new URL(window.location.href).searchParams.get("code"); // 인가코드 가져오기
-  console.log(code);
 
   // === state ===
+  // === 카카오 로그인 api ===
   const [apiConfig, setApiConfig] = useState({
     method: "get",
     url: `/user/oauth/kakao?code=${code}`
   });
+  // === 서버 세션 삭제 api ===
+  // const [apiConfig, setApiConfig] = useState({
+  //   method: "get",
+  //   url: `/redisFLUSHALL`
+  // });
   const { response, error, loading, fetchData } = useAxios(apiConfig, false);
 
   // === cookie ===
@@ -27,22 +32,53 @@ const Redirection = () => {
     fetchData();
   }, [apiConfig]);
 
+  // === 카카오 로그인 api ===
   useEffect(() => {
-    if (response !== null) {
-      debugger;
+    // reponse로 seeionId가 옴
+    // status 에 따른 분기점
+
+    if (response) {
       const userRole = response.authorityLevel === 100 ? "admin" : "member";
       const expires = new Date();
       expires.setTime(expires.getTime() + 7200 * 1000);
       setCookie("sessionId", response.sessionId, { path: "/", expires });
       setCookie("userRole", userRole, { path: "/", expires });
       navigate(`/`);
-    } else {
-      debugger;
-      navigate(`/member/login`);
     }
+
+    // if (response !== null) {
+    //   setCookie("sessionId", response.sessionId);
+    //   navigate(`/`);
+    // }
   }, [response, error]);
 
+  if (response !== null) {
+    const userRole = response.authorityLevel === 100 ? "admin" : "member";
+    const expires = new Date();
+    expires.setTime(expires.getTime() + 7200 * 1000);
+    setCookie("sessionId", response.sessionId, { path: "/", expires });
+    setCookie("userRole", userRole, { path: "/", expires });
+    navigate(`/`);
+  } else {
+    navigate(`/member/login`);
+  }
+
+  // === 서버 세션 삭제 api ===
+  // useEffect(() => {
+  //   console.log(response);
+
+  //   if (response !== null) {
+  //     navigate(`/`);
+  //   } else if (error) {
+  //     console.log(error);
+  //     navigate(`/member/login`);
+  //   } else {
+  //     navigate(`/member/login`);
+  //   }
+  // }, [response, error]);
+
   return <div>카카오 로그인 중...</div>;
+  // return <div>서버 세션 삭제 중...</div>;
 };
 
 export default Redirection;
